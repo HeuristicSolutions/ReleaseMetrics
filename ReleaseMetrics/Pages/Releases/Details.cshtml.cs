1cdc102a0a292682431fac93e326ec2290fd401a
Seth Petry-Johnson <seth@petry-johnson.com>
2019-06-28 12:05:42 -0400

    Added more ignored stuff

---
+++ ReleaseMetrics/Pages/Releases/Details.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Heuristics.Library.Extensions;
+using ReleaseMetrics.Core.Configuration;
+using ReleaseMetrics.Core.DataModel;
+using ReleaseMetrics.Core.Releases;
+using ReleaseMetrics.Pages.Shared;
+
+namespace ReleaseMetrics.Pages.Releases {
+
+	public class DetailsModel : BasePageModel {
+
+		public string ReleaseNumber { get; set; }
+
+		public DetailsModel(AppSettings config, MetricsDbContext db) : base(config, db) {
+		}
+
+		public void OnGet(string releaseNum) {
+			if (releaseNum.IsNullOrEmpty()) {
+				throw new ArgumentException("The 'releaseNum' parameter is required");
+			}
+
+			ReleaseNumber = releaseNum.Trim();
+		}
+	}
+}
