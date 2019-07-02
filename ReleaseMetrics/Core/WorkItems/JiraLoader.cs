17e39435d6b5e945151bfe53731cc6fe6b177379
Seth Petry-Johnson <seth@petry-johnson.com>
2019-07-02 14:04:45 -0400

    Finished handling jira stories w/ multiple major releases

--- ReleaseMetrics/Core/WorkItems/JiraLoader.cs
+++ ReleaseMetrics/Core/WorkItems/JiraLoader.cs
@@ -43,6 +43,22 @@ namespace ReleaseMetrics.Core.WorkItems {
 			var workItems = new List<WorkItem>();
 
 			foreach (var story in storiesInRelease) {
+				// TODO: To start tracking metrics for patch releases, will need to make this smarter 
+				var effectiveMajorRelease = GetPrimaryMajorRelease(story.FixVersions);
+
+				if (effectiveMajorRelease.IsNullOrEmpty()) {
+					messages.Add(
+						new ResultMsg($"Found 0 major release versions for {story.Id}, with fix versions '{story.FixVersions.Join(", ")}'", MessageType.Error)
+					);
+					continue;
+				}
+				else if (effectiveMajorRelease != releaseNum) {
+					messages.Add(
+						new ResultMsg($"{story.Id} is mapped to {releaseNum}, but its primary major release is {effectiveMajorRelease}. Jira stories should only be tracked to a single major release version.", MessageType.Warning)
+					);
+					continue;
+				}
+
 				try {
 					var epic = story.EpicStoryId.IsNotNullOrEmpty()
 						? await ApiProxy.GetStoryAsync(story.EpicStoryId)
@@ -82,6 +98,50 @@ namespace ReleaseMetrics.Core.WorkItems {
 			}
 
 			return (workItems, messages);
+		}
+
+		/// <summary>
+		/// stories should be tagged to at most 1 major release (x.y.0). If a story is tagged to multiple, 
+		/// we assign it to the EARLIEST version under the assumption that anything in x.y.0 is also in x.(y+1).0
+		/// </summary>
+		private string GetPrimaryMajorRelease(List<string> fixVersions) {
+			var candidateMajorReleases = fixVersions.Where(v => v.EndsWith(".0")).ToList();
+
+			if (candidateMajorReleases.Count == 0) {
+				return null;
+			}
+
+			else if (candidateMajorReleases.Count == 1) {
+				return candidateMajorReleases[0];
+			}
+
+			else {
+				var minMarketingReleaseNum = Int32.MaxValue;
+				var minMajorReleaseNum = Int32.MaxValue;
+
+				foreach (var releaseNum in candidateMajorReleases) {
+					try {
+						var versionComponents = releaseNum.Split(".");
+						var marketingRelease = versionComponents[0].ToInt32();
+						var majorRelease = versionComponents[1].ToInt32();
+
+						// 3.1.0 is earlier than 4.9.0
+						if (marketingRelease < minMarketingReleaseNum) {
+							minMarketingReleaseNum = marketingRelease;
+							minMajorReleaseNum = majorRelease;
+						}
+						// 3.1.0 is earlier than 3.2.0
+						else if (marketingRelease == minMarketingReleaseNum && majorRelease < minMajorReleaseNum) {
+							minMajorReleaseNum = majorRelease;
+						}
+					}
+					catch (Exception ex) {
+						throw new Exception($"Error parsing '{releaseNum}': " + ex.Message, ex);
+					}
+				}
+
+				return $"{minMarketingReleaseNum}.{minMajorReleaseNum}.0";
+			}
 		}
 	}
 }
